2b86369e6a397d4be82622bd302929a5e6c1239f
J Shamsul Bahri <jibone@gmail.com>
2014-04-25 12:04:25 +0800

    added default theme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  # Sets browser window title
+  def get_window_title(page_title)
+    base_title = "Webgram"
+    base_title = "#{base_title} | #{page_title}" if !page_title.empty?
+    base_title
+  end
+
+  # Sets meta author
+  def get_meta_author
+    # [TODO] get name from user model
+    "Wool and Candy"
+  end
+
+  # Sets meta description
+  def get_meta_description
+    # [TODO] get description from model
+    "Webgram Website...."
+  end
 end
--- app/themes/default/assets/javascripts/default/all.js
+++ app/themes/default/assets/javascripts/default/all.js
@@ -4,4 +4,7 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require_tree .
+//= require 'default/jquery.min'
+//= require 'default/bootstrap.min'
+//= require 'default/modernizr'
+//= require 'default/app'
